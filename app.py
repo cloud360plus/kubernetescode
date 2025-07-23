0abb07b444a6ebecbe0abe88c6f243e9f3877d5c
cloud360plus <cloud360plus@gmail.com>
2025-07-23 16:22:16 +0530

    Update app.py
        Shows the Pod name
    
        Shows the Pod IP
    
        Returns basic Pod info (using environment variables set by Kubernetes)

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+import os
+import socket
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+def pod_info():
+    pod_name = os.environ.get('HOSTNAME', 'Unknown')
+    pod_ip = socket.gethostbyname(socket.gethostname())
+
+    return f"""
+    <h2>Pod Information</h2>
+    <ul>
+        <li><strong>Pod Name:</strong> {pod_name}</li>
+        <li><strong>Pod IP:</strong> {pod_ip}</li>
+    </ul>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
